5570a6d8d83f747e6f1c46be6211f7c397e70b1f
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-26 09:53:22 +0100

    problem send

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+if (isset($_POST['submit']) && isset($_POST['mail']) && isset($_POST['message'])) {
+    $to= trim(strip_tags($_POST['mail']));
+    $message = trim(strip_tags($_POST['message']));
+    $subject = "Email a envoyé";
+
+    if (!filter_var($to,FILTER_VALIDATE_EMAIL)) {
+        if(strlen($message <= 500)) {
+            if (mail($to,$subject, $message)) {
+                echo "Email envoyé avec succès vers $to ...";
+            } else {
+                echo "Échec de l'envoi de l'email...";
+            }
+        }
+    }
+}?>
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -14,8 +31,9 @@
         <label for="mail"></label>
         <input type="email" name="mail" id="mail" placeholder="renseignez votre mail" required>
 
+
         <label for="message"></label>
-        <textarea name="message" id="message" cols="30" rows="10" minlength="15" maxlength="100" required></textarea>
+        <textarea name="message" id="message" cols="30" rows="10" maxlength="500" required></textarea>
 
         <button type="submit" name="submit">Envoyer</button>
     </form>
